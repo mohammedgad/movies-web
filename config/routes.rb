aabe5cd8cf7ac56eb924f7acb1babbccacaf2a0f
Mohammed Gad <mohammedgad7@gmail.com>
2018-11-13 22:36:17 +0200

    add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'movies#index'
+  get 'login', :to => "users#login", :as => "user_login"
+  get 'signup', :to => "users#create", :as => "user_create"
   get 'movies/index', :as => "movies"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
