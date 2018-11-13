ce3083ee5630ab01ccfc9c2e5e8951d0490e28ee
Mohammed Gad <mohammedgad7@gmail.com>
2018-11-14 00:14:02 +0200

    add user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
   def login
+    email = params[:email]
+    password = params[:password]
+    if email.present? && password.present?
+      r = HTTParty.post("http://0.0.0.0:3001/user_token",
+                :body => { "auth": {
+                  :email => email,
+                  :password => password
+                }}.to_json,
+                :headers => { 'Content-Type' => 'application/json' })
+      if r['jwt'].present?
+        session[:user_token] = r['jwt']
+        flash[:success] = "user loged in"
+      end
+    end
+  end
+
+  def logout
+    session[:user_token] = ""
   end
 
   def create
