20be621d06e095a68594778a2b7c26827f33f9d0
Mohammed Gad <mohammedgad7@gmail.com>
2018-11-13 22:51:31 +0200

    tmdb search api

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,8 @@
 class MoviesController < ApplicationController
   def index
+    if params[:search]
+      h = HTTParty.get("https://api.themoviedb.org/3/search/movie?api_key=#{ENV["TMDB_API_KEY"]}&query=#{params[:search]}")
+      @r = JSON.parse(h.body)
+    end
   end
 end
