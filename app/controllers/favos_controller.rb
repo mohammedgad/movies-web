4be091935db8bac96693d800b3fe78eeb4b58e03
Mohammed Gad <mohammedgad7@gmail.com>
2018-11-14 02:14:26 +0200

    add + remove favos

--- app/controllers/favos_controller.rb
+++ app/controllers/favos_controller.rb
@@ -10,8 +10,33 @@ class FavosController < ApplicationController
   end
 
   def create
+    if session[:user_token].present? && params[:movie_id]
+      h = HTTParty.post("http://0.0.0.0:3001/favos", headers: {
+       "Authorization" => "Bearer #{session[:user_token]}",
+           "cache-control" => "no-cache"
+        }, body: {
+        	"favo" => {
+        		"movie_id" => params[:movie_id],
+        		"movie_poster" => params[:movie_poster],
+        		"movie_overview" => params[:movie_overview],
+        		"movie_title" => params[:movie_title],
+        	}
+        })
+      if h
+        respond_to do |format|
+          format.js {render inline: "alert('#{h["movie_title"]} add to your Favos')" }
+        end
+      end
+    end
   end
 
   def destroy
+    if session[:user_token].present? && params[:favo_id]
+      h = HTTParty.delete("http://0.0.0.0:3001/favos/#{params[:favo_id]}", headers: {
+       "Authorization" => "Bearer #{session[:user_token]}",
+           "cache-control" => "no-cache"
+        })
+      redirect_back(fallback_location: root_path)
+    end
   end
 end
